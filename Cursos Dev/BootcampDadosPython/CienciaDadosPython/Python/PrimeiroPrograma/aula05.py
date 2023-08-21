7b06eb354d25b216d5c27ac6ab622595056c2d06
hallanbrito <hallanbrito@gmail.com>
2023-08-21 20:08:53 -0300

    adicionado aula de funçoes de entrada e saida no python

---
+++ Cursos Dev/BootcampDadosPython/CienciaDadosPython/Python/PrimeiroPrograma/aula05.py
@@ -0,0 +1,10 @@
+##funções de entradas e saida
+
+nome = input("informe o seu nome: ")
+idade = input("informe sua idade: ")
+print(f"seu nome é {nome}")
+print(f"Sua idade é {idade}")
+print(nome, idade)
+print(nome, idade, end="...")
+print(nome, idade, sep="#", end="...\n")
+print(nome, idade, sep="#")
