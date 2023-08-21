d5c8c0c02ed8463edcce5ddbc2cf2add10d996a3
hallanbrito <hallanbrito@gmail.com>
2023-08-21 07:57:55 -0300

    adicionado aula 3 sober variaveis no python

---
+++ Cursos Dev/BootcampDadosPython/CienciaDadosPython/Python/PrimeiroPrograma/aula03.py
@@ -0,0 +1,17 @@
+## Variaveis e constantes
+
+nome = "Hallan"
+idade = 34
+casado = True
+print(f"Nome: {nome}, Idade: {idade} casado: {casado}")
+
+
+nome, idade, casado = ("Rafael", 25, False)
+print(f"Nome: {nome}, Idade: {idade} casado: {casado}")
+
+## boas praticas
+
+# o padrão de nome sde ser snake case
+# escolher nomes sugestivos
+#Nome de constantes todo em maiúsculo.
+
