85e25aff2b46aa4af86d27944193ad6eb74830d2
hallanbrito <hallanbrito@gmail.com>
2023-08-20 22:18:37 -0300

    primeiras aulas de python no bootcamp

---
+++ Cursos Dev/BootcampDadosPython/CienciaDadosPython/Python/PrimeiroPrograma/aula01.py
@@ -0,0 +1,3 @@
+print("Hello World")
+print("Ola Mundo")
+print("oi, seja bem vindo ao curso de Python")
---
+++ Cursos Dev/BootcampDadosPython/CienciaDadosPython/Python/PrimeiroPrograma/aula02.py
@@ -0,0 +1,5 @@
+print(1 + 10)
+print(1.5 + 1 + 0.5)
+print(False)
+print(True)
+print("Python")
