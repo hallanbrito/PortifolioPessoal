2236725a0f5a9a39c35d1e9483af7430d3a1766c
hallanbrito <hallanbrito@gmail.com>
2023-08-25 21:36:41 -0300

    melhorado a organização das pastas

---
+++ Cursos Dev/BootcampDadosPython/CienciaDadosPython/Python/PrimeiroPrograma/aula12.py
@@ -0,0 +1,15 @@
+def exibir_mensagem():
+    print("Ola mundo!")
+
+def exibir_mensagem_2(nome):
+    print(f"Seja bem vindo {nome}!")
+
+def exibir_mensagem_3(nome="Anonomo"):
+    print(f"Seja bem vindo{nome}!")
+
+
+
+#exibir_mensagem()
+exibir_mensagem_2("Hallan")
+#exibir_mensagem_3()
+#exibir_mensagem_3(nome="Gulherme")
